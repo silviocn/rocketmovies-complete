4cae942b3c3ab93b0b925da8199711b715696dac
silvio <silviocostaadm@gmail.com>
2023-08-15 11:23:18 -0300

    uploading testing codes

---
+++ mytest.spec.js
@@ -0,0 +1,8 @@
+it("result of the sum of 2 + 2 must be 4", () => {// it receives two parameters: description and then the function
+  const a = 2;
+  const b = 2;
+  const result = a + b;
+
+  expect(result).toEqual(5); // what do you expect to happen
+
+});
